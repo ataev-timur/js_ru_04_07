74d577b360521e6bd294b6845324b064e42e3f99
ataev-timur <ataevtimur@gmail.com>
2017-08-04 09:49:44 +0300

    Some Fixes

--- src/reducer/comments.js
+++ src/reducer/comments.js
@@ -1,15 +1,34 @@
-import { ADD_COMMENT } from '../constants'
+import { ADD_COMMENT, LOAD_ARTICLE_COMMENTS, START, SUCCESS } from '../constants'
 import {normalizedComments as defaultComments} from '../fixtures'
 import { arrToMap } from '../helpers'
 import {Map} from 'immutable'
+import {Record} from 'immutable'
 
-export default (comments = arrToMap(defaultComments), action) => {
+const CommentRecord = Record({
+    id: null,
+    user: null,
+    text: '',
+})
+
+const ReducerState = Record({
+    entities: arrToMap(defaultComments, CommentRecord),
+    loading: false,
+    loaded: false
+})
+
+export default (commentsState = new ReducerState, action) => {
     const { type, payload, randomId } = action
 
     switch (type) {
         case ADD_COMMENT:
-            return comments.set(randomId, new Map(payload.comment))
+            return commentsState.setIn(['entities', randomId], new CommentRecord(payload.comment))
+
+        case LOAD_ARTICLE_COMMENTS + START:
+            return commentsState.setIn(['entities', payload.id, 'loading'], true)
+
+        case LOAD_ARTICLE_COMMENTS + SUCCESS:
+            return commentsState.setIn(['entities', payload.id], new ArticleRecord(response))
     }
 
-    return comments
+    return commentsState
 }
